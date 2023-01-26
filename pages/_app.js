48b38612c576ab4eb06891718f0195e796454f79
GakutoOkawara <ookawara0817@gmail.com>
2023-01-26 23:15:35 +0900

    add _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,37 @@
+import Link from "next/link"
+
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <div>
+        <header>
+          <h1>My Website</h1>
+        </header>
+
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/blog">Blog</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <main>
+          <Component {...pageProps} />
+        </main>
+
+        <footer>
+          <div>
+            Powered by <a href="https://nextjs.org/">Next.js</a>
+          </div>
+        </footer>
+      </div>
+    </div>
+    
+  )
 }
 
 export default MyApp
