fe11fd05fd958aca79562b3ec81628704e44de45
GakutoOkawara <ookawara0817@gmail.com>
2023-02-01 22:27:09 +0900

    fix blog/index.js to add getStaticProps

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,21 @@
+import fs from "fs"
+import path from "path"
+
 const BlogListPage = () => {
-  return <p>ここはブログ記事一覧ページです。</p>
+  return <p>ここはブログ記事一覧ページです</p>
+}
+
+export const getStaticProps = () => {
+  const dataDir = "data"
+  const fileNames = fs.readdirSync(dataDir)
+  const posts = fileNames.map(fileName => {
+    return path.join(dataDir, fileName)
+  })
+  console.log(posts)
+
+  return {
+    props: {}
+  }
 }
 
 export default BlogListPage
